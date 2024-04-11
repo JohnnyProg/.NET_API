413a5c177300489d9160a59723de5d91f8373954
JohnnyProg <jangryta9@gmail.com>
2024-04-11 17:38:30 +0200

    (init): First commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Lab9.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Lab9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserManager<IdentityUser> _user;
+        IConfiguration _configuration;
+        public UserController(UserManager<IdentityUser> manager, IConfiguration configuration) 
+        {
+            _user = manager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Token([FromBody] UserDto dto)
+        {
+            var user = await _user.FindByEmailAsync(dto.UserName);
+            var result = await _user.CheckPasswordAsync(user, dto.Password);
+
+            if (result)
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                };
+                foreach (var role in await _user.GetRolesAsync(user))
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var key = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["Tokens:Key"])
+                );
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Tokens:Issuer"],
+                    audience: _configuration["Tokens:Audience"],
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(60),
+                    signingCredentials: creds
+                );
+                return Ok(
+                    new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    }
+                );
+            }
+            else
+            {
+                return BadRequest("Login Failure");
+            }
+        }
+    }
+}
